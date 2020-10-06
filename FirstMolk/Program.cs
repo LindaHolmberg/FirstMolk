016fe7e823f63f9f7b7aaace24862e42f1a362e8
LindaHolmberg <69193585+LindaHolmberg@users.noreply.github.com>
2020-10-06 14:05:49 +0200

    Hello Mölk

--- FirstMolk/Program.cs
+++ FirstMolk/Program.cs
@@ -10,7 +10,7 @@ namespace FirstMolk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Mölk");
+            Console.WriteLine("Hello Mölk!");
             Console.ReadLine();
         }
     }
